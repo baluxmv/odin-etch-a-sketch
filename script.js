19ce47234a90dab56080628942959e1ace2dce35
Francisco <fmartinezv2002@gmail.com>
2024-07-12 11:20:52 -0400

    Add initial structure and 16x16 grid

---
+++ script.js
@@ -0,0 +1,9 @@
+// Element that represents the container of the grid
+const gridContainer = document.querySelector("#grid-container");
+
+const grid = size => {
+    for (i = 0; i < size * size; i++) {
+        let cell = document.createElement("div");
+        gridContainer.appendChild(cell);
+    }
+};

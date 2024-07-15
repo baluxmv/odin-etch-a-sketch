68020367d11fbdaaf9b0b026fee5c385d3ee7c82
Francisco <fmartinezv2002@gmail.com>
2024-07-15 12:50:35 -0400

    Add grid items styling and paint on hover

--- script.js
+++ script.js
@@ -2,10 +2,25 @@
 const gridContainer = document.querySelector("#grid-container");
 
 const grid = size => {
+    const gridItemWidth = 100 / size;
     for (i = 0; i < size * size; i++) {
-        let cell = document.createElement("div");
-        gridContainer.appendChild(cell);
+        let gridItem = document.createElement("div");
+        gridItem.classList.add("grid-item"); 
+        gridItem.style.width = `${gridItemWidth}%`;
+        gridItem.style.paddingBottom = `${gridItemWidth}%`
+        gridContainer.appendChild(gridItem);
     }
+
+    // Add event listener to each grid item 
+    const gridItems = document.querySelectorAll(".grid-item");
+    gridItems.forEach(gridItem => {
+        gridItem.addEventListener("mouseenter", () => {
+            gridItem.classList.add("painted");
+        });
+    });
 };
 
+
+
 grid(16);
+

e6c54abf95c53dba5d9c74b666ee70aa9be2f396
Francisco <fmartinezv2002@gmail.com>
2024-07-16 08:30:53 -0400

    Fix grid layout

--- script.js
+++ script.js
@@ -3,12 +3,21 @@ const gridContainer = document.querySelector("#grid-container");
 const resizeButton = document.querySelector("#resize-button");
 
 const grid = size => {
-    const gridItemWidth = 100 / size;
+    // Clear any previous existing grid items
+    while (gridContainer.firstChild) {
+            gridContainer.removeChild(gridContainer.firstChild);
+    }
+    
+    // Calculate the size of each grid item
+    const gridContainerSize = gridContainer.offsetWidth;
+    const gridItemSize = (gridContainerSize / size);
+    const gridItemPercent = gridItemSize / gridContainerSize * 100;
+
     for (i = 0; i < size * size; i++) {
         let gridItem = document.createElement("div");
         gridItem.classList.add("grid-item"); 
-        gridItem.style.width = `${gridItemWidth}%`;
-        gridItem.style.paddingBottom = `${gridItemWidth}%`
+        gridItem.style.flexBasis = gridItemPercent + "%";
+        
         gridContainer.appendChild(gridItem);
     }
 
@@ -21,7 +30,25 @@ const grid = size => {
     });
 };
 
+resizeButton.addEventListener("click", () => {
+    let integerInput;
 
+    do {
+        let userInput = prompt("How many number of squares per side?");
+        integerInput = parseInt(userInput);
+
+        if (Number.isInteger(integerInput) && 
+            1 <= integerInput && integerInput <= 100) {
+            break;
+        }
+        else {
+            alert("Invalid input, please enter an integer between 1 <= 100");
+        }
+    } 
+    while (true);
+    
+    grid(integerInput);
+});
 
 
 
